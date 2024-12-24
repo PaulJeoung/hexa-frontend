8df552026655f22977c3a9d0077a281fc3a4e52c
paul.jeoung <kuni5458@naver.com>
2024-12-25 02:26:20 +0900

    feature-home, main page, layout

--- src/components/common/ProductCarousel.js
+++ src/components/common/ProductCarousel.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import '../../App.css'
-import { Container, Carousel, Row, Col } from 'react-bootstrap'
-
+import { Button } from 'react-bootstrap'
 
 const ProductCarousel = ({ items, initialActive=0 }) => {
   const [active, setActive] = useState(initialActive)
@@ -30,30 +29,34 @@ const ProductCarousel = ({ items, initialActive=0 }) => {
   }
   return (
     <>
-        <Container>
-            <Row>
-                <Col md={6}>
-                    <div id="carousel" className='noselect'>
-                        <div className='arrow arrow-left' onClick={ moveLeft }>
-                            <i className='fl-arrow-left'></i>
-                        </div>
-                        <div className= {`transition-group ${direction}`}>
-                            { generateItem() }
-                        </div>
-                        <div className='arrow arrow-right' onClick={ moveRight }>
-                            <i className='fl-arrow-right'></i>
-                        </div>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+        <div className='d-flex justify-content-center'>
+            <div id="carousel" className='noselect'>
+                <div className='arrow arrow-left' onClick={ moveLeft }>
+                    <i className='fl-arrow-left'></i>
+                </div>
+                <div className= {`transition-group ${direction}`}>
+                    { generateItem() }
+                </div>
+                <div className='arrow arrow-right' onClick={ moveRight }>
+                    <i className='fl-arrow-right'></i>
+                </div>
+            </div>
+        </div>
+        <div className='text-center mt-5'>
+            <p><h4>타이틀</h4></p>
+            <p><h5>부제</h5></p>
+            <p><Button variant='secondary me-1'>상세보기</Button><Button variant='light ms-1'>구매하기</Button></p>
+        </div>
     </>
   )
 }
 
 const Item = ({ id, level }) => {
     const className = `item level${level}`
-    return <div className={className}>{id}</div>
+    return (
+        <div className={className}>
+            <img src={id} alt='신상이미지' className='carousel-image'/>
+        </div>)
 }
 
 export default ProductCarousel
